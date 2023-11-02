f6709ad010e4cb8227e0be7921a8d5fb94827ad2
Justin George <justingeorge322@gmail.com>
2023-11-02 23:53:13 +0530

    Allowed Railway as host

--- blogs/settings.py
+++ blogs/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blogs.wsgi.application'
 
-CSRF_TRUSTED_ORIGINS =['https://blog-production-b6b2.up.railway.app/']
+CSRF_TRUSTED_ORIGINS =['https://blog-production-b6b2.up.railway.app']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
