264f83a6e8a78b3bf40a990982bdd2dd3c58a705
Justin George <justingeorge322@gmail.com>
2023-11-25 22:25:18 +0530

    DEBUG=False

--- blogs/settings.py
+++ blogs/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'blog-phantasmagoria.azurewebsites.net', 'phantasmagoria.jadepy.tech']
 
